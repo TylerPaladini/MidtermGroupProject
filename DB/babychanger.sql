25ea78100987a158d7c3ab7f05f5f5b68297ffe8
rob10thHuman <rob@10thHuman.com>
2018-10-25 13:47:22 -0600

    committing

--- DB/babychanger.sql
+++ DB/babychanger.sql
@@ -16,14 +16,90 @@ CREATE SCHEMA IF NOT EXISTS `babychangerdb` DEFAULT CHARACTER SET utf8 ;
 USE `babychangerdb` ;
 
 -- -----------------------------------------------------
+-- Table `comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `comment` ;
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT NULL DEFAULT NULL,
+  `comment` VARCHAR(150) NULL DEFAULT NULL,
+  `restroom_id` INT NULL DEFAULT NULL,
+  `flag_comment` TINYINT(1) NULL DEFAULT NULL,
+  `rating` ENUM('1', '2', '3', '4', '5') NULL DEFAULT NULL,
+  `active` TINYINT(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
 -- Table `users`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `users` ;
 
 CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NULL DEFAULT NULL,
+  `last_name` VARCHAR(45) NULL DEFAULT NULL,
+  `username` VARCHAR(45) NULL DEFAULT NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  `password` VARCHAR(45) NULL DEFAULT NULL,
+  `active` TINYINT(1) NULL DEFAULT NULL,
+  `admin` TINYINT(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `location`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `location` ;
+
+CREATE TABLE IF NOT EXISTS `location` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NULL,
-  `last_name` VARCHAR(45) NULL,
+  `name` VARCHAR(45) NULL,
+  `address_id` INT NULL,
+  `access_limits` VARCHAR(45) NULL,
+  `purchase_required` TINYINT(1) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `address`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `address` ;
+
+CREATE TABLE IF NOT EXISTS `address` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `street` VARCHAR(45) NULL,
+  `street2` VARCHAR(45) NULL,
+  `city` VARCHAR(45) NULL,
+  `state` VARCHAR(45) NULL,
+  `zip` VARCHAR(45) NULL,
+  `phone` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `restroom`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `restroom` ;
+
+CREATE TABLE IF NOT EXISTS `restroom` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `condition` VARCHAR(45) NULL,
+  `location_id` INT NULL,
+  `picture` VARCHAR(45) NULL,
+  `flagged` TINYINT(1) NULL,
+  `flagged_reason` VARCHAR(45) NULL,
+  `flagged_date` DATETIME NULL,
+  `gender` ENUM('M', 'W', 'U', 'F') NULL,
+  `directions` VARCHAR(150) NULL,
+  `public` TINYINT(1) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
