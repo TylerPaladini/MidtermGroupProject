7fc04446b500a6cc54a617a99ea4bd77b205f7e3
Joshua Ciccone-Okodo <joshokodo@gmail.com>
2018-10-26 11:53:07 -0600

    updated comment dao

--- BabyChanger/src/main/java/com/skilldistillery/babychanger/data/CommentDAOImpl.java
+++ BabyChanger/src/main/java/com/skilldistillery/babychanger/data/CommentDAOImpl.java
@@ -17,22 +17,19 @@ import com.skilldistillery.babychanger.entities.Users;
 @Transactional
 @Repository
 public class CommentDAOImpl implements CommentDAO {
-	
+
 	private final String FIND_ALL = "SELECT c FROM Comment c";
 	private final String FIND_BY_USER_ID = "SELECT c FROM Comment c WHERE c.user.id = :id";
 	private final String FIND_BY_RESTROOM_ID = "SELECT c FROM Comment c WHERE c.restroom.id = :id";
-	private final String FIND_BY_KEYWORDS = "";
-	private final String FIND_BY_FLAG = "";
-	private final String FIND_BY_RATING = "";
-	private final String FIND_BY_ACTIVE = "";
-	private final String FIND_BY_DATE = "";
-	
+	private final String FIND_BY_COMMENT = "SELECT c FROM Comment c WHERE c.comment like :comment";
+	private final String FIND_BY_FLAG = "SELECT c FROM Comment c WHERE c.flagComment = :flag";
+	private final String FIND_BY_RATING = "SELECT c FROM Comment c WHERE c.rating = :rating";
+	private final String FIND_BY_ACTIVE = "SELECT c FROM Comment c WHERE c.active = :active";
+	private final String FIND_BY_DATE = "SELECT c FROM Comment c WHERE c.dateCreated = :date";
+
 	@PersistenceContext
 	private EntityManager em;
-	
-	
-	
-	
+
 	@Override
 	public List<Comment> findAll() {
 		return em.createQuery(FIND_ALL, Comment.class).getResultList();
@@ -42,12 +39,10 @@ public class CommentDAOImpl implements CommentDAO {
 	public Comment findCommentById(int id) {
 		return em.find(Comment.class, id);
 	}
-	
+
 	@Override
 	public List<Comment> findCommentsByUserId(int id) {
-		return em.createQuery(FIND_BY_USER_ID, Comment.class)
-				.setParameter("id", id)
-				.getResultList();
+		return em.createQuery(FIND_BY_USER_ID, Comment.class).setParameter("id", id).getResultList();
 	}
 
 	@Override
@@ -58,9 +53,7 @@ public class CommentDAOImpl implements CommentDAO {
 
 	@Override
 	public List<Comment> findCommentsByRestroomId(int id) {
-		return em.createQuery(FIND_BY_RESTROOM_ID, Comment.class)
-				.setParameter("id", id)
-				.getResultList();
+		return em.createQuery(FIND_BY_RESTROOM_ID, Comment.class).setParameter("id", id).getResultList();
 	}
 
 	@Override
@@ -76,52 +69,107 @@ public class CommentDAOImpl implements CommentDAO {
 	}
 
 	@Override
+	public List<Comment> findCommentsByComment(String comment) {
+		return em.createQuery(FIND_BY_COMMENT, Comment.class).setParameter("comment", comment).getResultList();
+	}
+
+	@Override
 	public List<Comment> findCommentsByFlagComment(Boolean flag) {
-		// TODO Auto-generated method stub
-		return null;
+		return em.createQuery(FIND_BY_FLAG, Comment.class).setParameter("flag", flag).getResultList();
 	}
 
 	@Override
 	public List<Comment> findCommentsByRating(Rating rating) {
-		// TODO Auto-generated method stub
-		return null;
+		int score = rating.getValue();
+		return em.createQuery(FIND_BY_RATING, Comment.class).setParameter("rating", score).getResultList();
 	}
 
 	@Override
 	public List<Comment> findCommentsByRating(int rating) {
-		// TODO Auto-generated method stub
-		return null;
+		return em.createQuery(FIND_BY_RATING, Comment.class).setParameter("rating", rating).getResultList();
 	}
 
 	@Override
 	public List<Comment> findCommentsByActive(Boolean active) {
-		// TODO Auto-generated method stub
-		return null;
+		return em.createQuery(FIND_BY_ACTIVE, Comment.class).setParameter("active", active).getResultList();
 	}
 
 	@Override
 	public List<Comment> findCommentsByDateCreated(Date date) {
-		// TODO Auto-generated method stub
-		return null;
+		return em.createQuery(FIND_BY_DATE, Comment.class).setParameter("date", date).getResultList();
+
 	}
 
 	@Override
-	public boolean addComment(Comment comment) {
-		// TODO Auto-generated method stub
-		return false;
+	public Comment addComment(Comment comment) {
+		em.persist(comment);
+		em.flush();
+		if (comment.getId() == 0) {
+			em.getTransaction().rollback();
+			return null;
+		} else {
+			return em.find(Comment.class, comment.getId());
+		}
 	}
 
 	@Override
-	public boolean editComment(int id, Comment comment) {
-		// TODO Auto-generated method stub
-		return false;
+	public Comment editComment(int id, Comment comment) {
+		Comment managed = em.find(Comment.class, id);
+
+		if (managed != null) {
+//			managed.setUser(comment.getUser());
+			managed.setComment(comment.getComment());
+			managed.setRating(comment.getRating());
+		}
+		return managed;
+	}
+
+	@Override
+	public boolean disableComment(int id) {
+		Comment managed = em.find(Comment.class, id);
+
+		if (managed != null && !managed.isActive()) {
+			managed.setActive(false);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean enableComment(int id) {
+		Comment managed = em.find(Comment.class, id);
+
+		if (managed != null && managed.isActive()) {
+			managed.setActive(true);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean deleteComment(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		Comment deleteComment = em.find(Comment.class, id);
+		em.remove(deleteComment);
+		if (em.find(Comment.class, id) != null) {
+			em.getTransaction().rollback();
+			return false;
+		} else {
+			return true;
+		}
 	}
-	
+
+	@Override
+	public boolean updateFlag(int id, boolean isFlag) {
+		Comment managed = em.find(Comment.class, id);
+
+		if (managed != null) {
+			managed.setFlagComment(isFlag);
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 }
