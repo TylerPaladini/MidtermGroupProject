a78b6d49764cd3b2d635093812e70ecaba8bde91
Tyler Paladini <TylerPaladini@yahoo.com>
2018-10-26 10:18:42 -0600

    User entity updated with temporal and User tests completed for mapping and add/remove

--- JPABabyChanger/src/test/java/com/skilldistillery/babychanger/entities/UsersTest.java
+++ JPABabyChanger/src/test/java/com/skilldistillery/babychanger/entities/UsersTest.java
@@ -2,6 +2,8 @@ package com.skilldistillery.babychanger.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Date;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -43,7 +45,34 @@ class UsersTest {
 	void test() {
 		Users user = em.find(Users.class, 1);
 		assertEquals("Jane", user.getFirstName());
-		assertEquals("janedoe", user.getEmail());
+		assertEquals("Doe", user.getLastName());
+		assertEquals("janedoe", user.getUserName());
+		assertEquals("janedoe@gmail.com", user.getEmail());
+		assertEquals(true, user.isActive());
+		assertEquals(null, user.getDateCreated());
+	}
+	
+	@Test
+	@DisplayName("test one to many with comments")
+	void test2() {
+		Comment comment = em.find(Comment.class, 1);
+		assertEquals("test comment", comment.getComment());
+		
+		
+		Users users = em.find(Users.class, 1);
+		assertEquals(2, users.getComments().size());
+		Comment comm = new Comment();
+		comm.setUser(em.find(Users.class, 2));
+		comm.setRestroom(em.find(Restroom.class, 1));
+		comm.setDateCreated(java.sql.Date.valueOf("2018-10-26"));
+		users.addComment(comm);
+		assertEquals(3, users.getComments().size());
+		users.removeComment(comm);
+		assertEquals(2, users.getComments().size());
+		
+		
+		
+		
 	}
 
 }
