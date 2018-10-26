f6c8145e3a695f6164784224b6558fe0d8bd2bda
z-lamb <lamb.zachary@gmail.com>
2018-10-25 18:50:58 -0600

    tests in location, need to work on time

--- JPABabyChanger/src/test/java/com/skilldistillery/babychanger/entities/LocationTest.java
+++ JPABabyChanger/src/test/java/com/skilldistillery/babychanger/entities/LocationTest.java
@@ -2,6 +2,9 @@ package com.skilldistillery.babychanger.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -42,6 +45,16 @@ class LocationTest {
 	@Test
 	void test() {
 		assertEquals("Solarium", location.getName());
+		assertEquals("have to be a student", location.getAccessLimits());
+		assertEquals(false, location.isPurchaseRequired());
+		assertEquals("7194406626", location.getPhone());
+//		LocalDate date = location.getOpenTime();
+//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ISO_LOCAL_TIME");
+//		String text = Integer.toString(location.getOpenTime().getHours());
+//		String text = date.format(formatter);
+		
+		
+		assertEquals("08:00:00", location.getOpenTime());
 	}
 
 }
