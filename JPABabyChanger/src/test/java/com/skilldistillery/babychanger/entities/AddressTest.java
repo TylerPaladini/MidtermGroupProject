bf8356ca3905b150802e85e3523729f2ad74f2cc
z-lamb <lamb.zachary@gmail.com>
2018-10-25 21:37:50 -0600

    addressTests passing

--- JPABabyChanger/src/test/java/com/skilldistillery/babychanger/entities/AddressTest.java
+++ JPABabyChanger/src/test/java/com/skilldistillery/babychanger/entities/AddressTest.java
@@ -1,7 +1,7 @@
 package com.skilldistillery.babychanger.entities;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -11,43 +11,46 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AddressTest {
 
-	private EntityManagerFactory emf;
+	private static EntityManagerFactory emf;
 
 	private EntityManager em;
+	
+	private Address address;
 
-//	@BeforeAll
-//	static void setUpBeforeClass() throws Exception {
-//	}
-//
-//	@AfterAll
-//	static void tearDownAfterClass() throws Exception {
-//	}
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	emf = Persistence.createEntityManagerFactory("babychangerdb");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
 	
 	@BeforeEach
 	public void setUp() throws Exception {
-		emf = Persistence.createEntityManagerFactory("babychangerdb");
 		em = emf.createEntityManager();
+		address = em.find(Address.class, 1);
 	}
 
 	@AfterEach
 	public void tearDown() throws Exception {
 		em.close();
-		emf.close();
 	}
-
-//	@Test
-//	void test() {
-//		fail("Not yet implemented");
-//	}
 	
 	@Test
+	@DisplayName("Test Address mappings")
 	void test_address_mappings() {
-		Location location = em.find(Location.class, 1);
-		assertEquals("1210 Toledo St", location.getAddress().getStreet()); 
+		assertEquals("1210 Toledo St", address.getStreet());
+		assertNull(address.getStreet2());
+		assertEquals("Henderson", address.getCity());
+		assertEquals("NV", address.getState());
+		assertEquals("80918", address.getZipCode());
 	}
 
 }
