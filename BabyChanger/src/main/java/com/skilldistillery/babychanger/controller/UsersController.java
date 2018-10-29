f7779f36e30cca75c6cf46848a0fd01d577f88a7
rob10thHuman <rob@10thHuman.com>
2018-10-29 13:35:29 -0600

    pulling updates

--- BabyChanger/src/main/java/com/skilldistillery/babychanger/controller/UsersController.java
+++ BabyChanger/src/main/java/com/skilldistillery/babychanger/controller/UsersController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
