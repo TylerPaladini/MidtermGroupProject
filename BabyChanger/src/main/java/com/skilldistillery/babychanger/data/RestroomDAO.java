7929e0530e005fa90dd018ba8530b59e9d11ae58
Tyler Paladini <TylerPaladini@yahoo.com>
2018-10-30 10:54:50 -0600

    Added show flagged restrooms in restroom DAO, added show flagged button in AdminProfile.jsp

--- BabyChanger/src/main/java/com/skilldistillery/babychanger/data/RestroomDAO.java
+++ BabyChanger/src/main/java/com/skilldistillery/babychanger/data/RestroomDAO.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.babychanger.data;
 
+import java.util.List;
+
 import com.skilldistillery.babychanger.entities.Restroom;
 
 public interface RestroomDAO {
@@ -8,4 +10,5 @@ public interface RestroomDAO {
 	public Restroom updateRestroom(int id, Restroom restroom); 
 	public boolean destroyRestroom(int id);
 	public boolean disableRestroom(int id, Restroom restroom);
+	public List<Restroom> getRestroomsByFlag(Boolean flag);
 }
